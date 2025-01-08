edde94a7e15c4fa8ee1862e90fcf45b368cbc885
Rhaísa Justo <163946245+RhaisaJusto@users.noreply.github.com>
2025-01-07 21:30:04 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# BookConnect
-Projeto realizado para a empresa Cia do Livro, onde desenvolvemos na faculdade um app mobile em JAVA.
+# 2024-01-Desafio_2-Android_Developer
+
+Atividade avaliativa para a disciplina Android Developer - Construindo seu 1º App, do Curso de Análise e Desenvolvimento de Sistemas, do Centro Universitário de Valença.
+
+> Status: Finalizado
+
+<!-- > Previsão de entrega: 30/06/2024 -->
+
+## Objetivo
+
+O objetivo primordial deste desafio é conceber e implementar um programa para dispositivo móvel robusto e amigável para uma livraria fictícia. Além disso, buscar promover a experiência do usuário, considerando a navegabilidade, o design atraente e a facilidade de acesso às informações.
+O aplicativo será chamado BookConnect (Aplicativo para estabelecer vínculos entre a livraria e seus clientes)
+
+## Tecnologias utilizadas
+
+<img src="https://img.icons8.com/?size=100&id=123603&format=png&color=000000" width="50" alt="React Native" title="React Native"> <img src="https://img.icons8.com/?size=100&id=IpN1evivrDWO&format=png&color=000000" width="50" alt="Expo" title="Expo"> <img src="https://reactnavigation.org/img/spiro.svg" width="50" alt="React Navigator" title="React Navigator"> <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmiro.medium.com%2Fv2%2Fresize%3Afit%3A318%2F1*7jRD5QhgARucFKvRHFxpOg.png&f=1&nofb=1&ipt=997e398fe330bf8d04cf8852250b830e0312dd75b7224ae849d70b9b5a6dec51&ipo=images" height="50" alt="Styled Components" title="Styled Components">
+
+## Colaboradores
+
+Este projeto foi desenvolvido graças aos
+<a href="https://github.com/laisbrme/BookConnect/graphs/contributors"> colaboradores.</a>
+
+<a href="https://github.com/laisbrme"><img src="https://avatars.githubusercontent.com/u/52614305?s=64&v=4" height="50" /> Laís Brum</a>
+
+<a href="https://github.com/RhaisaJusto"><img src="https://avatars.githubusercontent.com/u/163946245?s=64&v=4" height="50" /> Rhaísa Justo</a>
+
+<a href="https://github.com/laisbrme"><img src="https://avatars.githubusercontent.com/u/163802559?s=64&v=4" height="50" /> Leonardo Pontes</a>
+
+## Vídeo de apresentação
+
+[![Watch the video](https://img.youtube.com/vi/WKR7pHkXfps/hqdefault.jpg)]( https://youtu.be/WKR7pHkXfps)
---
+++ routes.js
@@ -0,0 +1,168 @@
+// 1º Importando as screens
+import Login from "./src/screens/Login";
+import Home from "./src/screens/Home";
+import News from "./src/screens/News";
+import Favorites from "./src/screens/Favorites";
+import Events from "./src/screens/Events";
+import Reading from "./src/screens/Reading";
+import SelectedBook from "./src/screens/SelectedBook";
+import Comments from "./src/screens/Comments";
+import RewriteEnding from "./src/screens/RewriteEnding";
+import UnderConstruction from "./src/screens/UnderConstruction";
+import Splash from "./src/screens/Splash";
+
+// 2º Criando a navegação
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import { theme } from "./src/styles/theme";
+import {
+	Entypo,
+	MaterialCommunityIcons,
+	MaterialIcons,
+} from "@expo/vector-icons";
+
+// Navs
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+// const Drawer = createDrawerNavigator();
+
+export const StackNavigate = () => {
+	return (
+		<Stack.Navigator initialRouteName="Splash">
+			<Stack.Screen
+				name="Splash"
+				component={Splash}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name="Login"
+				component={Login}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name="Home"
+				component={TabNavigate}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name="SelectedBook"
+				component={SelectedBook}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name="Comments"
+				component={Comments}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name="RewriteEnding"
+				component={RewriteEnding}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name="UnderConstruction"
+				component={UnderConstruction}
+				options={{ headerShown: true }}
+			/>
+		</Stack.Navigator>
+	);
+};
+
+export const TabNavigate = () => {
+	return (
+		<Tab.Navigator initialRouteName="Home">
+			<Tab.Screen
+				name="Início"
+				component={Home}
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<Entypo
+							name="home"
+							size={30}
+							color={
+								focused ? theme.colors.ICONE_ATIVO : theme.colors.ICONE_INATIVO
+							}
+						/>
+					),
+					tabBarActiveTintColor: theme.colors.ICONE_ATIVO,
+					tabBarInactiveTintColor: theme.colors.ICONE_INATIVO,
+					headerShown: false,
+				}}
+			/>
+			<Tab.Screen
+				name="Leituras"
+				component={Reading}
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<MaterialIcons
+							name="book"
+							size={30}
+							color={
+								focused ? theme.colors.ICONE_ATIVO : theme.colors.ICONE_INATIVO
+							}
+						/>
+					),
+					tabBarActiveTintColor: theme.colors.ICONE_ATIVO,
+					tabBarInactiveTintColor: theme.colors.ICONE_INATIVO,
+					headerShown: false,
+				}}
+			/>
+			<Tab.Screen
+				name="Novidades"
+				component={News}
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<MaterialCommunityIcons
+							name="alert-decagram"
+							size={30}
+							color={
+								focused ? theme.colors.ICONE_ATIVO : theme.colors.ICONE_INATIVO
+							}
+						/>
+					),
+					tabBarActiveTintColor: theme.colors.ICONE_ATIVO,
+					tabBarInactiveTintColor: theme.colors.ICONE_INATIVO,
+					headerShown: false,
+				}}
+			/>
+			<Tab.Screen
+				name="Eventos"
+				component={Events}
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<MaterialIcons
+							name="event-available"
+							size={30}
+							color={
+								focused ? theme.colors.ICONE_ATIVO : theme.colors.ICONE_INATIVO
+							}
+						/>
+					),
+					tabBarActiveTintColor: theme.colors.ICONE_ATIVO,
+					tabBarInactiveTintColor: theme.colors.ICONE_INATIVO,
+					headerShown: false,
+				}}
+			/>
+			<Tab.Screen
+				name="Favoritos"
+				component={Favorites}
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<MaterialCommunityIcons
+							name="cards-heart"
+							size={30}
+							color={
+								focused ? theme.colors.ICONE_ATIVO : theme.colors.ICONE_INATIVO
+							}
+						/>
+					),
+					tabBarActiveTintColor: theme.colors.ICONE_ATIVO,
+					tabBarInactiveTintColor: theme.colors.ICONE_INATIVO,
+					headerShown: false,
+				}}
+			/>
+		</Tab.Navigator>
+	);
+};
